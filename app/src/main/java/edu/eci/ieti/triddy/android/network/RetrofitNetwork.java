f58330f2007c37323040cbdf86d038ccd6d52ee5
Ricar8o <andres.martinez-d@mail.escuelaing.edu.co>
2021-05-12 18:33:57 -0500

    Add Logout

--- app/src/main/java/edu/eci/ieti/triddy/android/network/RetrofitNetwork.java
+++ app/src/main/java/edu/eci/ieti/triddy/android/network/RetrofitNetwork.java
@@ -11,7 +11,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitNetwork {
 
-    private static final String BASE_URL = "http://10.0.2.2:8080/";
+    private static final String BASE_URL = "http://ec2-34-203-184-51.compute-1.amazonaws.com:8080";
     private AuthService authService;
 
     public RetrofitNetwork() {
