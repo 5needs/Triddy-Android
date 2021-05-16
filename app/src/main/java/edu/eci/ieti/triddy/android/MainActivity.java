9602ef2f990377f3be5713e91a2688b7a413fe02
Edyesid <54111499+Edyesid@users.noreply.github.com>
2021-05-16 15:53:22 -0500

    :+1:

--- app/src/main/java/edu/eci/ieti/triddy/android/MainActivity.java
+++ app/src/main/java/edu/eci/ieti/triddy/android/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.product_register);
+        setContentView(R.layout.product_edit);
     }
 }
