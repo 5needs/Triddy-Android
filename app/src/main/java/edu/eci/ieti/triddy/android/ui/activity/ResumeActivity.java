0d269da70f4d6c6186d5cc2c5f6c68fd3113848d
David Andres Vargas Leon <47030575+GEARSHORDA3@users.noreply.github.com>
2021-05-20 21:58:13 -0500

    manage triddy options view

---
+++ app/src/main/java/edu/eci/ieti/triddy/android/ui/activity/ResumeActivity.java
@@ -0,0 +1,20 @@
+package edu.eci.ieti.triddy.android.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import edu.eci.ieti.triddy.android.R;
+
+public class ResumeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resume);
+    }
+}
